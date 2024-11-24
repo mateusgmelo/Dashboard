a2ff2d6459b3fe3f0db001465dade950eeb2ae19
Vítorio Esoindola <vae@discente.ifpe.edu.br>
2024-11-23 21:07:01 -0300

    Alterei a barra de busca

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 // Header.js
+import './Header.css'
+import lupa from '../assets/lupa.png';
 const Header = () => {
   return (
     <div className="header">
-      <input type="text" placeholder="Search" />
+      <form className="search-container">
+      <input type="text" id="search-bar" placeholder="Busca" />
+      <a href="#">
+        <img
+          className="search-icon"
+          src={lupa}
+          alt="Ícone de busca"
+        />
+      </a>
+    </form>
       <div className="profile">Farmácia Cavalcante</div>
     </div>
   );
