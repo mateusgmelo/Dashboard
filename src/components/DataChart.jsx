1534e258791c8113f4e82b54b7368474f557ebb8
Vítorio Esoindola <vae@discente.ifpe.edu.br>
2024-11-25 10:07:44 -0300

    Alterei o grafico

--- src/components/DataChart.jsx
+++ src/components/DataChart.jsx
@@ -1,25 +1,38 @@
-// DataChart.js
-import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
 
 const data = [
-  { name: '25°', hours: 24, days: 22 },
-  { name: '25,6°', hours: 26, days: 23 },
-  // Adicione mais dados conforme necessário
+  { hora: '08:00', gerada: 30, esperada: 40 },
+  { hora: '09:00', gerada: 50, esperada: 55 },
+  { hora: '10:00', gerada: 45, esperada: 50 },
+  { hora: '11:00', gerada: 60, esperada: 65 },
+  { hora: '12:00', gerada: 70, esperada: 75 },
+  { hora: '13:00', gerada: 90, esperada: 95 },
+  { hora: '14:00', gerada: 100, esperada: 110 }
 ];
 
 const DataChart = () => {
   return (
-    <div className="data-chart">
-      <BarChart width={300} height={200} data={data}>
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis dataKey="month" label={{ value: 'Hora', position: 'insideBottom', offset: -5 }} />
+        <YAxis label={{ value: 'Energia (kWh)', angle: -90, position: 'insideLeft' }} />
         <Tooltip />
-        <Bar dataKey="hours" fill="#8884d8" />
-        <Bar dataKey="days" fill="#82ca9d" />
-      </BarChart>
-    </div>
+        <Legend verticalAlign="top" />
+        <Line type="monotone" dataKey="gerada" name="Energia Gerada" stroke="#00366b" />
+        <Line type="monotone" dataKey="esperada" name="Energia Gerada Esperada" stroke="#ff9900" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
