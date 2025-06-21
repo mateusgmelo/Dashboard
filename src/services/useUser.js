b22ce57d8dad0e89b86655ff524e460f6ab7d829
Vítorio Esoindola <vae@discente.ifpe.edu.br>
2025-06-21 15:36:31 -0300

    Pequena alteração

--- src/services/useUser.js
+++ src/services/useUser.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import api from '../services/api'; // ajuste o caminho conforme seu projeto
+import api from '../services/api';
 
 export function useUser() {
   const [user, setUser] = useState(null);
