7e9e2502bfc3fd5e7582c8edad1c12387b2c0cbd
Mateus Gonçalves melo <mateusgmeloo@gmail.com>
2025-06-26 10:40:54 -0300

    mateus

---
+++ src/pages/Suporte.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Suporte() {
+  // O componente agora só se preocupa com o seu próprio conteúdo.
+  return (
+    <div>
+      <h1>Página de Suporte</h1>
+      <p>Bem-vindo à nossa central de suporte...</p>
+    </div>
+  );
+}
+
+export default Suporte;
